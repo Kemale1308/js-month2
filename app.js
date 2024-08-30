99c495486c52a8f9ab4dfefddd11b9bf3ac076ba
Kemale1308 <esgerovak@gmail.com>
2024-08-30 19:17:43 +0400

    date

---
+++ app.js
@@ -0,0 +1,30 @@
+
+let month = prompt("Zehmet olmasa istediyiniz ayin nomresini yazin :");
+
+if (month === "1") {
+    console.log("yanvar");
+} else if (month === "2") {
+    console.log("fevral");
+} else if (month === "3") {
+    console.log("mart");
+} else if (month === "4") {
+    console.log("aprel");
+} else if (month === "5") {
+    console.log("may");
+} else if (month === "6") {
+    console.log("iyun");
+} else if (month === "7") {
+    console.log("iyul");
+} else if (month === "8") {
+    console.log("avqust");
+} else if (month === "9") {
+    console.log("sentyabr");
+} else if (month === "10") {
+    console.log("oktyabr");
+} else if (month === "11") {
+    console.log("noyabr");
+} else if (month === "12") {
+    console.log("dekabr");
+} else {
+    console.log(" Netice gosterilmir xais edirik 1 den 12 ye qeder reqm daxil edesiz . Cunki 12 Ayimiz var");
+}
